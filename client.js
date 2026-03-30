9a170068d8cf624cfd88c63297f9d20791df10d4
David Maccari <maccari.davi06@gmail.com>
2026-03-30 11:50:29 -0300

    Renomeação de arquivo e adição do parametro body

--- client.js
+++ client.js
@@ -1,13 +1,13 @@
 const axios = require("axios");
 
-async function consomeRota(URL, path, metodo) {
+async function consomeRota(URL, path, metodo, body) {
   let response = "";
   switch (metodo) {
     case "get":
       response = await axios.get(URL + path);
       break;
     case "post":
-      response = await axios.post(URL + path);
+      response = await axios.post(URL + path, body);
       break;
   }
   return response;
