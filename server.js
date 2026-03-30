673901d2c6aef619b5ab1a5061d48345e846a9f2
David Maccari <maccari.davi06@gmail.com>
2026-03-30 11:24:04 -0300

    Adicionando servidor

---
+++ server.js
@@ -0,0 +1,28 @@
+const fastify = require("fastify")({ logger: true });
+
+fastify.get("/procurar/:id", async (request, reply) => {
+  const { id } = request.params;
+  return id;
+});
+
+fastify.get("/validate", async (request, reply) => {
+  const { nome, idade } = request.query
+  if (idade >= 18){
+    return `O usuário "${nome}" é maior de idade`
+  } else if (idade < 18){
+    return `O usuário "${nome}" é menor de idade`
+  }
+  return "Teste sem usuário"
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+    console.log("Server running on http://localhost:3000");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
